4ec1b966791cb4f2e35f8b2430d9acee83041b67
ivanero1102 <ivanero1102@gmail.com>
2022-10-04 18:02:18 +0200

    HEcho

---
+++ Practica1.php
@@ -0,0 +1,7 @@
+<?php
+
+    $numero1 = (int)$_POST['numero1'];
+    $numero2 = (int)$_POST['numero2'];
+    echo $numero1;
+    echo $numero2;
+?>
--- Referencia_valor.php
+++ Referencia_valor.php
@@ -10,8 +10,14 @@
     $prueba1 = 5;
     $prueba2 = 5;
 
+    echo "referencia: $prueba1 </br>";
+    echo "valor: $prueba2 </br>";
+
+    echo "Se ejecuta la funcion referencia que resta 1 </br>";
     referencia($prueba1);
+    echo "Se ejecuta la funcion valor que resta 1 </br>";
     valor($prueba2);
-    echo "$prueba1 </br>";
-    echo $prueba2;
+
+    echo "referencia: $prueba1 </br>";
+    echo "valor: $prueba2";
 ?>
