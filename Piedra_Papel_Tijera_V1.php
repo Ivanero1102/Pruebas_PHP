89e1e5395defe5e37299e1b6a93c7b5149377fe0
ivanero1102 <ivanero1102@gmail.com>
2022-10-07 11:53:16 +0200

    Toques finales

--- Piedra_Papel_Tijera_V1.php
+++ Piedra_Papel_Tijera_V1.php
@@ -16,6 +16,12 @@
         ?>
     </head>
     <body>
+        <div class="contenedor">
+            <h1>Piedra Papel o Tijera</h1>
+        </div>
+        <div class="contenedor">
+            <h3>Mejor de 5</h3>
+        </div>
         <div class="marcador">
                     <table>
                         <tr>
@@ -60,47 +66,63 @@
             </form>
         </div>
         <?php
+            echo "<div class='contenedor'>";
+            echo "JUGADOR: " .$_SESSION['eleccion_jugador']. " <br>";
+            echo "MAQUINA: " .$_SESSION['eleccion_maquina']. " <br>";
+         if ($_SESSION['eleccion'] == $_SESSION['random']) {
+            echo "empate";
+            }else{
+            if(($_SESSION['eleccion']==1 and $_SESSION['random'] ==2) or ($_SESSION['eleccion']==2 and $_SESSION['random'] ==3) or ($_SESSION['eleccion']==3 and $_SESSION['random'] ==1)){
+                echo "perdiste";
+            }else{
+                echo "ganaste";
+            }
+            echo "</div>";
+            }
              if(isset($_POST['PPT'])){
-                $eleccion = (int)$_POST['PPT'];
-                $random = rand(1,3);
-                $eleccion_maquina;
-                $eleccion_jugador;
-                switch ($random) {
+                $_SESSION['eleccion'] = (int)$_POST['PPT'];
+                $_SESSION['random'] = rand(1,3);;
+                $_SESSION['eleccion_maquina']= "";
+                $_SESSION['eleccion_jugador']= "";
+                switch ($_SESSION['random']) {
                     case 1:
-                        $eleccion_maquina = "Piedra";
+                        $_SESSION['eleccion_maquina'] = "Piedra";
                         break;
                     case 2:
-                        $eleccion_maquina = "Papel";
+                        $_SESSION['eleccion_maquina'] = "Papel";
                         break;
                     case 3:
-                        $eleccion_maquina = "Tijera";
+                        $_SESSION['eleccion_maquina'] = "Tijera";
                         break;
                 }
-                switch ($eleccion) {
+                switch ($_SESSION['eleccion']) {
                     case 1:
-                        $eleccion_jugador = "Piedra";
+                        $_SESSION['eleccion_jugador'] = "Piedra";
                         break;
                     case 2:
-                        $eleccion_jugador = "Papel";
+                        $_SESSION['eleccion_jugador'] = "Papel";
                         break;
                     case 3:
-                        $eleccion_jugador = "Tijera";
+                        $_SESSION['eleccion_jugador'] = "Tijera";
                         break;
                 }
-                echo "JUGADOR: $eleccion_jugador <br>";
-                echo "MAQUINA: $eleccion_maquina <br>";
-                if ($eleccion == $random) {
-                    echo "empate";
+                if ($_SESSION['eleccion'] == $_SESSION['random']) {
                 }else{
-                    if(($eleccion==1 and $random ==2) or ($eleccion==2 and $random ==3) or ($eleccion==3 and $random ==1)){
-                        echo "perdiste";
+                    if(($_SESSION['eleccion']==1 and $_SESSION['random'] ==2) or ($_SESSION['eleccion']==2 and $_SESSION['random'] ==3) or ($_SESSION['eleccion']==3 and $_SESSION['random'] ==1)){
                         punto_maquina($_SESSION['maquina']);
                     }else{
-                        echo "ganaste";
                         punto_jugador($_SESSION['jugador']);
                     }
                 }
-             }
+                if ($_SESSION['maquina']== 3 ) {
+                        header("location:/Piedra_Papel_Tijera_Perdiste.php");
+                }else{
+                    if ($_SESSION['jugador'] == 3 ) {
+                        header("location:/Piedra_Papel_Tijera_Ganaste.php");
+                    }
+                }
+                header("location:Piedra_Papel_Tijera_V1.php");
+            }
         ?>
     </body>
 </html>
