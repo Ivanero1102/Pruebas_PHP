b33a8e44aab0c9e06c13525f1d151bd88b00e56c
ivanero1102 <ivanero1102@gmail.com>
2022-10-19 21:41:45 +0200

    Prueba

---
+++ JavaScripo.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicio1</title>
+    </head>
+    <body>
+        <!-- <script src="JavaScript.js"></script> -->
+        <?php
+        $num = 0;
+        ?>  
+        <script type="text/javascript"> 
+        function prueba(){
+            var hi = 4 
+            var e = "<?php echo $num ?>";
+            alert (e);
+            var a = 9;
+            window.location.href = window.location.href + "?w1 =" + a;
+        }
+        </script>
+        <?php
+        session_start();
+        if(!isset($_SESSION['count'])){
+            $_SESSION['count']= 0;
+            echo "<script> prueba() </script>";
+        }else{
+            $_SESSION['count']++;
+        } 
+
+        if(isset($_GET["w1"])){
+            $prueba = $_GET["w1"];
+            echo $prueba;
+        }
+        ?>
+        <script type="text/javascript"> 
+        function prueba(){
+            var hi = 4 
+            var e = "<?php echo $num ?>";
+            alert (e);
+            var a = 9;
+            window.location.href = window.location.href + "?w1 =" + a;
+        }
+        </script>
+    </body>
+</html>
